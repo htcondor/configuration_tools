fe9be4d2ce0589e0dea91f5b68b3428a8384a796
Robert Rati <rrati@redhat.com>
2010-05-11 08:31:45 -0500

    - Created test suite for the configd - Added additional logging to configd - Fixed issues processing WallabyConfigEvents

---
+++ test/store.rb
@@ -0,0 +1,171 @@
+#!/usr/bin/ruby
+
+require 'spqr/spqr'
+require 'spqr/app'
+require 'rhubarb/rhubarb'
+
+module Mrg
+   module Grid
+      module Config
+         class Store
+            include ::SPQR::Manageable
+            qmf_package_name 'mrg.grid.config'
+            qmf_class_name 'Store'
+
+            def Store.find_by_id(u)
+               @singleton ||= Store.new
+            end
+
+            def Store.find_all
+               @singleton ||= Store.new
+               [@singleton]
+            end
+#
+#            def addNode(name)
+#               node = Node.find_first_by_name(name)
+#               unless node
+#                  node = Node.create(:name => name, :last_updated_version => 0)
+#               end
+#               node
+#            end
+#
+#            expose :addNode do |args|
+#               args.declare :obj, :objId, :out, {}
+#               args.declare :name, :sstr, :in, {}
+#            end
+
+            def getNode(name)
+               node = Node.find_first_by_name(name)
+               unless node
+                  node = Node.create(:name => name, :last_updated_version => 0)
+               end
+               @node_name ||= name
+               node
+            end
+
+            expose :getNode do |args|
+               args.declare :obj, :objId, :out, {}
+               args.declare :name, :sstr, :in, {}
+            end
+
+            def checkNodeValidity(name)
+               node = Node.find_first_by_name(name)
+               node
+            end
+
+            expose :checkNodeValidity do |args|
+               args.declare :name, :sstr, :in, {}
+               args.declare :obj, :objId, :out, {}
+            end
+
+            def raiseEvent(targets, need_restart, subsystems)
+               node_map = {}
+               targets.each do |name|
+                  if name == @node_name
+                     node = Node.find_first_by_name(name)
+                     node_map[name] = node.last_updated_version
+                  else
+                     node_map[name] = 0
+                  end
+               end
+               event = WallabyConfigEvent.new(node_map, need_restart, {'condor' => subsystems})
+#               event.affectedNodes = node_map
+#               event.restart = need_restart
+#               event.targets = {'condor' => subsystems}
+               event.bang!
+            end
+
+            expose :raiseEvent do |args|
+               args.declare :targets, :list, :in, :desc=>"A map of targets:version"
+               args.declare :need_restart, :bool, :in, :desc=>"Whether to restart the subsystem"
+               args.declare :subsystems, :list, :in, :desc=>"A map of system:subsystem that should be acted upon"
+            end
+         end
+
+         class Node
+            include ::Rhubarb::Persisting
+            include ::SPQR::Manageable
+            qmf_package_name 'mrg.grid.config'
+            qmf_class_name 'Node'
+
+            declare_column :name, :string
+            declare_column :last_checkin, :integer
+            declare_column :last_updated_version, :integer
+
+
+            alias def_last_checkin last_checkin
+            alias def_last_updated_version last_updated_version
+
+            def last_checkin
+              def_last_checkin || 0
+            end
+
+            def last_updated_version
+              def_last_updated_version || 0
+            end
+
+            qmf_property :name, :lstr, :index=>true
+            qmf_property :last_checkin, :uint64
+            qmf_property :last_updated_version, :uint64
+
+            def getConfig(version)
+#               config["WALLABY_CONFIG_VERSION"] = self.last_updated_version.to_s
+               config["WALLABY_CONFIG_VERSION"] = version
+               config
+            end
+
+            expose :getConfig do |args|
+               args.declare :version, :uint64, :in, {}
+               args.declare :config, :map, :out, {}
+            end
+
+            def setLastUpdatedVersion(version)
+               self.last_updated_version = version
+            end
+
+            expose :setLastUpdatedVersion do |args|
+               args.declare :version, :uint64, :in, {}
+            end
+
+            def checkin()
+               self.last_checkin = ::Rhubarb::Util::timestamp
+            end
+
+            expose :checkin do |args|
+            end
+
+            private
+            def config
+               # XXX: put config here
+               @config ||= {}
+            end
+         end
+
+         class WallabyConfigEvent
+            include ::SPQR::Raiseable
+            arg :affectedNodes, :map, ""
+            arg :restart, :bool, ""
+            arg :targets, :map, ""
+
+            qmf_class_name :WallabyConfigEvent
+            qmf_package_name :WallabyConfigEvent
+            qmf_severity :notice
+         end
+      end
+   end
+end
+
+
+options = {}
+options[:user] = "guest"
+options[:password] = "guest"
+options[:host] = "127.0.0.1"
+options[:port] = 5672
+
+Rhubarb::Persistence::open(":memory:")
+Mrg::Grid::Config::Node.create_table
+
+app = SPQR::App.new(options)
+app.register Mrg::Grid::Config::Store,Mrg::Grid::Config::Node,Mrg::Grid::Config::WallabyConfigEvent
+
+app.main
---
+++ test/test_driver.py
@@ -0,0 +1,240 @@
+#!/usr/bin/python
+#   Copyright 2008 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import os
+import sys
+import time
+import datetime
+from condorutils.osutil import run_cmd
+from condorutils.readconfig import read_condor_config
+from qmf.console import Session
+from wallabyclient import WallabyHelpers
+
+nodename = 'unit_test'
+checkin_time = 30
+config_file = './condor_config.configd'
+log_file = './configd.log'
+
+try:
+   os.remove(config_file)
+except:
+   pass
+try:
+   os.remove(log_file)
+except:
+   pass
+
+# Start the broker
+print 'Starting the broker'
+broker_pid = os.fork()
+if broker_pid == 0:
+   run_cmd('qpidd')
+   sys.exit(0)
+
+time.sleep(2)
+
+# Start the store
+print 'Starting the store'
+store_pid = os.fork()
+if store_pid == 0:
+   env = {}
+   if 'RUBYLIB' in os.environ.keys():
+      env['RUBYLIB'] = os.environ['RUBYLIB']
+   run_cmd('./store.rb', environ = env)
+   print 'About to exit store child thread'
+   sys.exit(0)
+
+time.sleep(2)
+
+# Start the configd
+print 'Starting configd'
+configd_pid = os.fork()
+if configd_pid == 0:
+   env = {}
+   if 'PYTHONPATH' in os.environ.keys():
+      env['PYTHONPATH'] = os.environ['PYTHONPATH']
+   env['CONDOR_CONFIG'] = '../config/99configd.config'
+   env['_CONDOR_QMF_CONFIGD_CHECK_INTERVAL'] = str(checkin_time)
+   env['_CONDOR_QMF_BROKER_HOST'] = '127.0.0.1'
+   env['_CONDOR_QMF_BROKER_PORT'] = '5672'
+   (rcode, out, err) = run_cmd('../condor_configd -l %s -m %s -h %s' % (log_file, config_file, nodename), environ = env)
+   sys.exit(0)
+
+# Setup the connection to the store
+session = Session()
+session.addBroker('amqp://127.0.0.1:5672')
+store = []
+try:
+   store = session.getObjects(_class='Store', _package='mrg.grid.config')
+except Exception, error:
+   print 'Error: %s' % error
+
+if store == []:
+   print 'Unable to contact Configuration Store'
+else:
+   store = store[0]
+
+# Wait until the Node object is created in the store, which means the configd
+# has started up and contacted the store
+result = store.checkNodeValidity(nodename)
+while result.status != 0:
+   time.sleep(1)
+   result = store.checkNodeValidity(nodename)
+
+# Get the node object
+result = store.getNode(nodename)
+if result.status != 0:
+  print 'Error: Failed to retrieve node object'
+  sys.exit(1)
+else:
+   node = session.getObjects(_objectId=result.outArgs['obj'])[0]
+
+#try:
+   # Test 1 - Test the initial checkin
+   print 'Testing initial checkin: \t\t\t',
+
+   # The configd waits between 0-10 seconds before initial checking, so wait 12
+   # seconds to verify configd has checked in with the store for the first time
+   time.sleep(12)
+
+   node.update()
+   if node.last_checkin > 0:
+      print 'PASS'
+   else:
+      print 'FAILED'
+
+   # Test 2 - Verify a new config version is in the config file (should be 0
+   # since this would be the first checkin)
+   print 'Verifying config file pulled from store: \t\t',
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = -1
+   if version == 0:
+      print 'PASS'
+   else:
+      print 'FAILED (%d!=0)' % version
+
+   # Test 3 - Verify configd checkins in with the store periodically
+   print 'Testing periodic checkin: \t\t\t\t',
+   old_checkin = node.last_checkin
+   time.sleep(checkin_time+1)
+   node.update()
+   if old_checkin < node.last_checkin:
+      print 'PASS (%d < %d)' % (old_checkin, node.last_checkin)
+   else:
+      print 'FAILED (%d !< %d)' % (old_checkin, node.last_checkin)
+
+   # Test 4 - Test config retrieval if new version found at wallaby
+   print 'Testing periodic checkin retrieves new config: \t\t',
+   node.setLastUpdatedVersion(version+1)
+   time.sleep(checkin_time+1)
+   old_version = version
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = old_version
+   if old_version != version:
+      print 'PASS (%d != %d)' % (version, old_version)
+   else:
+      print 'FAILED (%d == %d)' % (version, old_version)
+
+   # Test 5 - Verify older config version causes config retrieval
+   print 'Testing older version causes config retrieval: \t\t',
+   node.setLastUpdatedVersion(version-1)
+   time.sleep(checkin_time+1)
+   old_version = version
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = old_version
+   if old_version != version:
+      print 'PASS (%d != %d)' % (version, old_version)
+   else:
+      print 'FAILED (%d == %d)' % (version, old_version)
+
+   # Test 6 - Test event (1 target) causes config file download
+   print 'Testing event with single target causes config retrieval: \t',
+   old_version = version
+   node.setLastUpdatedVersion(version+1)
+   store.raiseEvent([nodename], True, ['master'])
+   time.sleep(1)
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = old_version
+   if old_version != version:
+      print 'PASS (%d != %d)' % (version, old_version)
+   else:
+      print 'FAILED (%d == %d)' % (version, old_version)
+
+   # Test 7 - Test event (>1 target) causes config file download
+   print 'Testing event with mulitple targets causes config retrieval: \t',
+   old_version = version
+   node.setLastUpdatedVersion(version+1)
+   store.raiseEvent([nodename, 'node1', 'node2'], True, ['master'])
+   time.sleep(1)
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = old_version
+   if old_version != version:
+      print 'PASS (%d != %d)' % (version, old_version)
+   else:
+      print 'FAILED (%d == %d)' % (version, old_version)
+
+   # Test 8 - Test event (1 target & >1 subsys) causes config file download
+   print 'Testing event with 1 target multiple subsystems causes config retrieval: \t',
+   old_version = version
+   node.setLastUpdatedVersion(version+1)
+   store.raiseEvent([nodename], True, ['master', 'startd', 'schedd'])
+   time.sleep(1)
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = old_version
+   if old_version != version:
+      print 'PASS (%d != %d)' % (version, old_version)
+   else:
+      print 'FAILED (%d == %d)' % (version, old_version)
+
+   # Test 9 - Test event (>1 target & >1 subsys) causes config file download
+   print 'Testing event with >1 target mulitple subsystems causes config retrieval: \t',
+   old_version = version
+   node.setLastUpdatedVersion(version+1)
+   store.raiseEvent([nodename, 'node1', 'node2'], True, ['master', 'schedd'])
+   time.sleep(1)
+   try:
+      version = int(read_condor_config('WALLABY_CONFIG', ['VERSION'], environ={'CONDOR_CONFIG':config_file})['version'])
+   except:
+      print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
+      version = old_version
+   if old_version != version:
+      print 'PASS (%d != %d)' % (version, old_version)
+   else:
+      print 'FAILED (%d == %d)' % (version, old_version)
+
+#except Exception, error:
+#   print 'Error: Exception raised: %s' % error
+#   os.killpg(os.getpgid(store_pid), 15)
+
+# Shut everything down
+os.killpg(os.getpgid(store_pid), 15)

b66d00c86251d39768e981827a760794a704e4a2
Peter MacKinnon <pmackinn@redhat.com>
2010-06-10 15:33:38 -0400

    Wrong syntax in copy command

--- config/pidkill.bat
+++ config/pidkill.bat
@@ -3,7 +3,7 @@ echo off
 REM leave a pid file so that a non-DC process
 REM like configd can be notified for shutdown
 
-copy /y /nul > %cd%\.pid%1
+copy /y nul > %cd%\.pid%1
 
 REM set this sleep value to be at least twice
 REM as long as the QMF_CONFIGD_WIN_INTERVAL

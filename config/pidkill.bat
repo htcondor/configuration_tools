b6c77e0210fa747b8fcbb7fc8762d6a4ab85b4b7
Timothy St. Clair <tstclair@redhat.com>
2010-06-10 15:02:13 -0500

    Update for ping wait to use invalid address so timeouts are reached.

--- config/pidkill.bat
+++ config/pidkill.bat
@@ -11,14 +11,14 @@ set cntr=0
 
 :LoopStart
 IF NOT EXIST %cd%\.pid%1 Goto EndClean
-ping 127.0.0.1 -n 1 -w 1000 > nul
+ping 1.1.1.1 -n 1 -w 1000 > nul
 IF %cntr%==5 Goto EndBad
 set /A cntr=%cntr%+1
 Goto LoopStart
 
 :EndClean
 REM Give an extra second to clean up
-ping 127.0.0.1 -n 1 -w 1000 > nul
+ping 1.1.1.1 -n 1 -w 1000 > nul
 EXIT /B 0
 
 :EndBad

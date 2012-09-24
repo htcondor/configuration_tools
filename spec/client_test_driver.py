f2478c9907ea1c7d9056f9845328ceedd779f276
Robert Rati <rrati@redhat.com>
2012-09-24 17:35:29 -0400

    The configd will exit if it detects more than 1 store.  Added test for this scenario

--- spec/client_test_driver.py
+++ spec/client_test_driver.py
@@ -18,6 +18,8 @@ import sys
 import time
 import datetime
 import shutil
+import signal
+import shlex
 from condorutils.osutil import run_cmd
 from condorutils.readconfig import read_condor_config
 from qmf.console import Session
@@ -25,7 +27,7 @@ from wallabyclient import WallabyHelpers
 from wallabyclient.exceptions import WallabyStoreError
 
 nodename = 'unit_test'
-checkin_time = 60
+checkin_time = 10
 config_file = './condor_config.configd'
 log_file = './configd.log'
 override_dir = './override'
@@ -44,10 +46,12 @@ except:
 print 'Starting the broker'
 broker_pid = os.fork()
 if broker_pid == 0:
-   run_cmd('qpidd')
+   cmd = shlex.split('qpidd --log-to-stdout no --log-to-stderr no --log-to-syslog yes')
+   os.execvp(cmd[0], cmd)
+   print "about to exit broker"
    sys.exit(0)
 
-time.sleep(2)
+time.sleep(5)
 
 # Start the store
 print 'Starting the store'
@@ -56,26 +60,24 @@ if store_pid == 0:
    env = {}
    if 'RUBYLIB' in os.environ.keys():
       env['RUBYLIB'] = os.environ['RUBYLIB']
-   run_cmd('./store.rb', environ = env)
+   cmd = shlex.split('./store.rb')
+   os.execve(cmd[0], cmd, env)
    print 'About to exit store child thread'
    sys.exit(0)
 
-time.sleep(2)
-
-# Start condor
-print 'Starting condor'
-condor_pid = os.fork()
-if condor_pid == 0:
+print 'Starting a second store'
+bad_store_pid = os.fork()
+if bad_store_pid == 0:
    env = {}
-   env['CONDOR_CONFIG'] = '/etc/condor/condor_config'
-   env['_CONDOR_DAEMON_LIST'] = 'MASTER'
-   env['_CONDOR_LOG'] = '.'
-   env['_CONDOR_LOCK'] = '.'
-   (rcode, out, err) = run_cmd('condor_master', environ = env)
+   if 'RUBYLIB' in os.environ.keys():
+      env['RUBYLIB'] = os.environ['RUBYLIB']
+   cmd = shlex.split('./store.rb')
+   os.execve(cmd[0], cmd, env)
+   print 'About to exit 2nd store child thread'
    sys.exit(0)
 
-# Start the configd
 print 'Starting configd'
+print 'Testing exit upon multiple configuration stores detected: \t',
 configd_pid = os.fork()
 if configd_pid == 0:
    env = {}
@@ -88,12 +90,62 @@ if configd_pid == 0:
    env['_CONDOR_QMF_BROKER_PORT'] = '5672'
    env['_CONDOR_CONFIGD_OVERRIDE_DIR'] = override_dir
    env['_CONDOR_LOG'] = '.'
-   (rcode, out, err) = run_cmd('../condor_configd -d -l %s -m %s -h %s' % (log_file, config_file, nodename), environ = env)
+   env['PATH'] = os.environ['PATH']
+   cmd = shlex.split('../condor_configd -d -l %s -m %s -h %s' % (log_file, config_file, nodename))
+   os.execve(cmd[0], cmd, env)
+   print 'Exiting 1st configd pass'
+   sys.exit(0)
+
+# Give the config time to detect 2 configuration stores
+time.sleep(20)
+
+# Check if the configd process is still there
+status = os.waitpid(configd_pid, os.WNOHANG)
+if status != (0, 0):
+   if os.WIFEXITED(status[1]) and os.WEXITSTATUS(status[1]) != 0 and status[0] == configd_pid:
+      print 'PASS'
+   else:
+      print 'FAILED (Process exit not correct type "%d")' % os.WEXITSTATUS(status[1])
+else:
+   print 'FAILED (Configd still running)'
+
+os.kill(bad_store_pid, 9)
+
+# Connect to the broker
+session = Session()
+session.addBroker('amqp://127.0.0.1:5672')
+
+# Wait until there is only 1 store running
+while True:
+   cnt = 0
+   o = session.getAgents()
+   for a in o:
+      if a.label == 'com.redhat.grid.config:Store':
+         cnt += 1
+   if cnt == 1:
+      break
+   else:
+      time.sleep(1)
+
+# Start the configd
+configd_pid = os.fork()
+if configd_pid == 0:
+   env = {}
+   if 'PYTHONPATH' in os.environ.keys():
+      env['PYTHONPATH'] = os.environ['PYTHONPATH']
+   env['CONDOR_CONFIG'] = '/etc/condor/condor_config'
+   env['_CONDOR_LOCAL_CONFIG_DIR'] = '../config'
+   env['_CONDOR_CONFIGD_CHECK_INTERVAL'] = str(checkin_time)
+   env['_CONDOR_QMF_BROKER_HOST'] = '127.0.0.1'
+   env['_CONDOR_QMF_BROKER_PORT'] = '5672'
+   env['_CONDOR_CONFIGD_OVERRIDE_DIR'] = override_dir
+   env['_CONDOR_LOG'] = '.'
+   env['PATH'] = os.environ['PATH']
+   cmd = shlex.split('../condor_configd -d -l %s -m %s -h %s' % (log_file, config_file, nodename))
+   os.execve(cmd[0], cmd, env)
    sys.exit(0)
 
 # Setup the connection to the store
-session = Session()
-session.addBroker('amqp://127.0.0.1:5672')
 store = []
 try:
    (agent, store) = WallabyHelpers.get_store_objs(session)
@@ -101,33 +153,32 @@ except Exception, error:
    print 'Error: %s' % error.error_str
 
 if store == []:
-   os.killpg(os.getpgid(store_pid), 9)
-   os.killpg(os.getpgid(configd_pid), 9)
-   os.killpg(os.getpgid(broker_pid), 9)
-   run_cmd('killall condor_master')
+   os.kill(store_pid, 9)
+   os.kill(configd_pid, 9)
+   os.kill(broker_pid, 9)
    sys.exit(1)
 
 # Wait until the Node object is created in the store, which means the configd
 # has started up and contacted the store
-result = store.checkNodeValidity(nodename)
-while result.status != 0:
+print "Waiting for the node object to be created in the store"
+result = store.checkNodeValidity([nodename])
+while result.invalidNodes != []:
    time.sleep(1)
-   result = store.checkNodeValidity(nodename)
+   result = store.checkNodeValidity([nodename])
 
 # Get the node object
 result = store.getNode(nodename)
 if result.status != 0:
    print 'Error: Failed to retrieve node object'
-   os.killpg(os.getpgid(store_pid), 9)
-   os.killpg(os.getpgid(configd_pid), 9)
-   os.killpg(os.getpgid(broker_pid), 9)
-   run_cmd('killall condor_master')
+   os.kill(store_pid, 9)
+   os.kill(configd_pid, 9)
+   os.kill(broker_pid, 9)
    sys.exit(1)
 else:
    node = agent.getObjects(_objectId=result.outArgs['obj'])[0]
 
 try:
-   # Test 1 - Test the initial checkin
+   # Test the initial checkin
    print 'Testing initial checkin: \t\t\t\t\t',
 
    # The configd waits between 0-10 seconds before initial checkin and can take
@@ -141,7 +192,7 @@ try:
    else:
       print 'FAILED'
 
-   # Test 2 - Verify a new config version is in the config file (should be 0
+   # Verify a new config version is in the config file (should be 0
    # since this would be the first checkin)
    print 'Verifying config file pulled from store: \t\t\t',
    try:
@@ -154,7 +205,7 @@ try:
    else:
       print 'FAILED (%d!=0)' % version
 
-   # Test 3 - Verify configd checks in with the store periodically
+   # Verify configd checks in with the store periodically
    print 'Testing periodic checkin: \t\t\t\t\t',
    old_checkin = node.last_checkin
    time.sleep(checkin_time+5)
@@ -171,7 +222,7 @@ try:
    except:
       old_value = 0
 
-   # Test 4 - Test config retrieval if new version found at wallaby
+   # Test config retrieval if new version found at wallaby
    print 'Testing periodic checkin retrieves new config: \t\t\t',
    node.setLastUpdatedVersion(version+1)
    time.sleep(checkin_time+5)
@@ -186,7 +237,7 @@ try:
    else:
       print 'FAILED (%d == %d)' % (version, old_version)
 
-   # Test 5 - Test files in the override directory are included in the config
+   # Test files in the override directory are included in the config
    print 'Testing override directory files override values in config: \t',
    try:
       new_value = read_condor_config('CONFIGD', ['TEST_PARAM'], environ={'CONDOR_CONFIG':config_file}, permit_param_only = False)['test_param']
@@ -197,7 +248,30 @@ try:
    else:
       print 'FAILED (%s == %s)' % (old_value, new_value)
 
+   # Restart the configd with no periodic checkin
+   os.kill(configd_pid, 9)
+   configd_pid = os.fork()
+   if configd_pid == 0:
+      env = {}
+      if 'PYTHONPATH' in os.environ.keys():
+         env['PYTHONPATH'] = os.environ['PYTHONPATH']
+      env['CONDOR_CONFIG'] = '/etc/condor/condor_config'
+      env['_CONDOR_LOCAL_CONFIG_DIR'] = '../config'
+      env['_CONDOR_CONFIGD_CHECK_INTERVAL'] = '0'
+      env['_CONDOR_QMF_BROKER_HOST'] = '127.0.0.1'
+      env['_CONDOR_QMF_BROKER_PORT'] = '5672'
+      env['_CONDOR_CONFIGD_OVERRIDE_DIR'] = override_dir
+      env['_CONDOR_LOG'] = '.'
+      env['PATH'] = os.environ['PATH']
+      cmd = shlex.split('../condor_configd -d -l %s -m %s -h %s' % (log_file, config_file, nodename))
+      os.execve(cmd[0], cmd, env)
+      sys.exit(0)
+
+   # The configd performs a checkin within 10 seconds on startup
+   time.sleep(15)
+
    # Add features and groups to the node
+   WallabyHelpers.add_group(session, store, 'TestGroup')
    grp_name = WallabyHelpers.get_id_group_name(node, session)
    node_grp = WallabyHelpers.get_group(session, store, grp_name)
    node.modifyMemberships('add', ['TestGroup'], {})
@@ -205,10 +279,11 @@ try:
    node.update()
    node_grp.update()
 
-   # Test 6 - Verify older config version causes config retrieval
+   # Verify older config version causes config retrieval
    print 'Testing older version causes config retrieval: \t\t\t',
    node.setLastUpdatedVersion(version-1)
-   time.sleep(checkin_time+5)
+   store.raiseEvent([nodename])
+   time.sleep(10)
    old_version = version
    try:
       version = int(read_condor_config('WALLABY', ['CONFIG_VERSION'], environ={'CONDOR_CONFIG':config_file}, permit_param_only = False)['config_version'])
@@ -220,7 +295,7 @@ try:
    else:
       print 'FAILED (%d == %d)' % (version, old_version)
 
-   # Test 7 - Test WallabyGroups is updated
+   # Test WallabyGroups is updated
    print 'Testing WallabyGroups was updated: \t\t\t\t',
    try:
       value = read_condor_config('', ['WallabyGroups'], environ={'CONDOR_CONFIG':config_file})['wallabygroups']
@@ -231,7 +306,7 @@ try:
    else:
       print 'FAILED (%s)' % value
 
-   # Test 8 - Test WallabyFeatures is updated
+   # Test WallabyFeatures is updated
    print 'Testing WallabyFeatures was updated: \t\t\t\t',
    try:
       value = read_condor_config('', ['WallabyFeatures'], environ={'CONDOR_CONFIG':config_file})['wallabyfeatures']
@@ -248,12 +323,12 @@ try:
    node.update()
    node_grp.update()
 
-   # Test 9 - Test event (1 target) causes config retrieval
+   # Test event (1 target) causes config retrieval
    print 'Testing event (1 target) causes config retrieval: \t\t',
    old_version = version
    node.setLastUpdatedVersion(version+1)
    store.raiseEvent([nodename])
-   time.sleep(8)
+   time.sleep(10)
    try:
       version = int(read_condor_config('', ['WALLABY_CONFIG_VERSION'], environ={'CONDOR_CONFIG':config_file})['wallaby_config_version'])
    except:
@@ -264,7 +339,7 @@ try:
    else:
       print 'FAILED (%d == %d)' % (version, old_version)
 
-   # Test 10 - Test WallabyGroups is updated
+   # Test WallabyGroups is updated
    print 'Testing WallabyGroups was updated: \t\t\t\t',
    try:
       value = read_condor_config('', ['WallabyGroups'], environ={'CONDOR_CONFIG':config_file})['wallabygroups']
@@ -275,7 +350,7 @@ try:
    else:
       print 'FAILED (%s)' % value
 
-   # Test 11 - Test WallabyFeatures is updated
+   # Test WallabyFeatures is updated
    print 'Testing WallabyFeatures was updated: \t\t\t\t',
    try:
       value = read_condor_config('', ['WallabyFeatures'], environ={'CONDOR_CONFIG':config_file})['wallabyfeatures']
@@ -286,33 +361,33 @@ try:
    else:
       print 'FAILED (%s)' % value
 
-   # Test 12 - Test event (>1 target) causes config retrieval
+   # Test event (>1 target) causes config retrieval
    print 'Testing event (>1 targets) causes config retrieval: \t\t',
    old_version = version
    node.setLastUpdatedVersion(version+1)
    store.raiseEvent([nodename, 'node1', 'node2'])
-   time.sleep(8)
+   time.sleep(10)
    try:
       version = int(read_condor_config('', ['WALLABY_CONFIG_VERSION'], environ={'CONDOR_CONFIG':config_file})['wallaby_config_version'])
    except:
       print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
-      version = old_version
+      version = -1
    if old_version != version:
       print 'PASS (%d != %d)' % (version, old_version)
    else:
       print 'FAILED (%d == %d)' % (version, old_version)
 
-   # Test 13 - Test event not for this node does not cause a config retrieval
+   # Test event not for this node does not cause a config retrieval
    print 'Testing not all events cause config retrieval: \t\t\t',
    old_version = version
    node.setLastUpdatedVersion(version+1)
    store.raiseEvent(['node1', 'node2'])
-   time.sleep(8)
+   time.sleep(10)
    try:
       version = int(read_condor_config('', ['WALLABY_CONFIG_VERSION'], environ={'CONDOR_CONFIG':config_file})['wallaby_config_version'])
    except:
       print 'Error: Failed to find WALLABY_CONFIG_VERSION in config file'
-      version = old_version
+      version = -1
    if old_version == version:
       print 'PASS (%d == %d)' % (version, old_version)
    else:
@@ -322,13 +397,11 @@ try:
 
 except Exception, error:
    print 'Error: Exception raised: %s' % error
-   os.killpg(os.getpgid(store_pid), 9)
-   os.killpg(os.getpgid(configd_pid), 9)
-   os.killpg(os.getpgid(broker_pid), 9)
-   run_cmd('killall condor_master')
+   os.kill(store_pid, 9)
+   os.kill(configd_pid, 9)
+   os.kill(broker_pid, 9)
 
 # Shut everything down
-os.killpg(os.getpgid(configd_pid), 9)
-os.killpg(os.getpgid(store_pid), 9)
-os.killpg(os.getpgid(broker_pid), 9)
-run_cmd('killall condor_master')
+os.kill(configd_pid, 9)
+os.kill(store_pid, 9)
+os.kill(broker_pid, 9)
